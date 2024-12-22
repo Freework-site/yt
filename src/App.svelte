63b913df693bc436c26fe8ea802ba8dc27c61f51
Freework-site <amstv2223@gmail.com>
2024-12-22 12:24:22 +0500

    final

--- src/App.svelte
+++ src/App.svelte
@@ -22,7 +22,7 @@
         loading = true;
 
         try {
-            const response = await axios.get('http://127.0.0.1:8000/api/video-info/', {
+            const response = await axios.get('http://192.168.100.20:8000/api/video-info/', {
                 params: { url },
             });
             title = response.data.title;
